ff9345ef5e57411842cfd51eea4c969dc8fd5d6c
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-10-26 16:54:14 +0300

    submit form.pssword error, more than 5, handlesubmit, prevent

--- src/components/SingUpForm.vue
+++ src/components/SingUpForm.vue
@@ -1,69 +1,80 @@
 <template>
-  <form>
-    <label>email:</label>
-    <input type="email" required v-model="email" />
+  <form @submit.prevent="handleSubmit">
+    <label>Email:</label>
+    <input type="email" v-model="email" required>
 
-    <label>password:</label>
-    <input type="password" required v-model="password" />
+    <label>Password:</label>
+    <input type="password" v-model="password" required>
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
-    <label>Skills:</label>
-    <input type="text" required v-model="tempSkills" @keypress="addSkill" />
-    <div v-for="skill in skills" :key="skill" class="pill">
-      <span v-on:click="deleteSkills(skill)">{{ skill }}</span>
-    </div>
-
-    <label>role:</label>
+    <label>Role:</label>
     <select v-model="role">
-      <option value="developer">developer</option>
-      <option value="webDesigner">webDesigner</option>
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
     </select>
 
-    <div class="terms">
-      <input type="checkbox" v-model="terms" required />
-      <label>Accapt terms and conditions</label>
+    <label>Skills (press alt + comma to add):</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
-  </form>
-  <p>email: {{ email }}</p>
-  <p>password: {{ password }}</p>
-  <p>role: {{ role }}</p>
-  <p>terms accepted: {{ terms }}</p>
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
 
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: "",
-      password: "",
-      role: "designer",
-      terms: true,
-      tempSkills: "",
+      email: '',
+      password: '',
+      role: 'developer',
+      terms: false,
       skills: [],
-    };
-
+      tempSkill: '',
+      passwordError: null,
+    }
   },
   methods: {
-    addSkill(e) {
-      if (e.key === "," && this.tempSkills) {
-        //we cannot add duplicate skills
-        if (!this.skills.includes(this.tempSkills)) {
-          this.skills.push(this.tempSkills);
+    addSkill($event) {
+      if($event.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill)
         }
-        this.tempSkills = "";
+        this.tempSkill = ''
       }
     },
-    deleteSkills(skill) {
-      this.skills = this.skills.filter((item) => {
-        return skill = !item;
-      });
+    deleteSkill(skill) {
+      this.skills = this.skills.filter(item => {
+        return skill !== item
+      })
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError = this.password.length > 5 ?
+        '' : 'Password must be at least 6 characters long'
+      if (!this.passwordError) {
+        // make request to database to save user
+        console.log('email: ', this.email)
+        console.log('password: ', this.password)
+        console.log('role: ', this.role)
+        console.log('skills: ', this.skills)
+        console.log('terms accepted: ', this.terms)
+      }
     }
   }
-};
+}
 </script>
 
-<style scoped>
+<style>
 form {
   max-width: 420px;
   margin: 30px auto;
@@ -72,7 +83,6 @@ form {
   padding: 40px;
   border-radius: 10px;
 }
-
 label {
   color: #aaa;
   display: inline-block;
@@ -82,7 +92,6 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-
 input, select {
   display: block;
   padding: 10px 6px;
@@ -92,7 +101,6 @@ input, select {
   border-bottom: 1px solid #ddd;
   color: #555;
 }
-
 input[type="checkbox"] {
   display: inline-block;
   width: 16px;
@@ -100,7 +108,6 @@ input[type="checkbox"] {
   position: relative;
   top: 2px;
 }
-
 .pill {
   display: inline-block;
   margin: 20px 10px 0 0;
@@ -113,7 +120,6 @@ input[type="checkbox"] {
   color: #777;
   cursor: pointer;
 }
-
 button {
   background: #0b6dff;
   border: 0;
@@ -122,16 +128,13 @@ button {
   color: white;
   border-radius: 20px;
 }
-
 .submit {
   text-align: center;
 }
-
 .error {
   color: #ff0062;
   margin-top: 10px;
   font-size: 0.8em;
   font-weight: bold;
 }
-
 </style>
